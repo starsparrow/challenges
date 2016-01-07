6e5609e4f300388a8f3a9495d917a0785050bb37
starsparrow <nik.spurgetis@gmail.com>
2016-01-07 13:15:05 -0500

    Added synacor-challenge folder; finished AoC 6-1 after refactoring light list

--- advent-of-code/day6/puz1.py
+++ advent-of-code/day6/puz1.py
@@ -23,22 +23,37 @@ for i in range(0,1000000):
     
 # Return the lightid given the x,y coordinates of the light
 def getlightid(coord):
-    unpackedcoord = coord.split(',')
-    x = int(unpackedcoord[0])
-    y = int(unpackedcoord[1])
+    x = coord[0]
+    y = coord[1]
     lightid = (1000 * y) + x
     return lightid
 
-# Takes lightid of corner lights and returns a list of lightids for the square
-## FIXME: RETURNS WAY TOO MANY LIGHTS BECAUSE I AM DUMB
+# Takes coords of corner lights and returns list of lightids for the rectangle
 def getlightlist(corner1, corner2):    
-    lightlist = []
-    if corner1 <= corner2:
-        for i in range(corner1, corner2 + 1):
-            lightlist.append(i)
-    else:
-        print('Error! corner 1 greater than corner 2. Clean up input file.')
-    return lightlist
+    unpackedcorner1 = corner1.split(',')
+    unpackedcorner2 = corner2.split(',')
+    
+    xmin = int(unpackedcorner1[0])
+    xmax = int(unpackedcorner2[0])
+    ymin = int(unpackedcorner1[1])
+    ymax = int(unpackedcorner2[1])
+    xcoordlist = []
+    ycoordlist = []  
+    for i in range(xmin, xmax + 1):
+        xcoordlist.append(i)    
+    for i in range(ymin, ymax + 1):
+        ycoordlist.append(i)
+    
+    lightcoordlist = []
+    for y in ycoordlist:
+        for x in xcoordlist:
+            lightcoordlist.append([x, y])
+    
+    lightidlist = []
+    for lightcoord in lightcoordlist:
+        lightidlist.append(getlightid(lightcoord))
+    
+    return lightidlist
     
 # Take a lightid and manipulate it according to the supplied command
 def operatelight(lightid, command):
@@ -50,33 +65,11 @@ def operatelight(lightid, command):
         lightgrid[lightid] = abs(lightgrid[lightid] - 1)
     else:
         print('Error! Invalid command "%s"' % command)
-
-        
+ 
 # Execute all of the commands
-for command in testcommands:
-    lights = getlightlist(getlightid(command[1]), getlightid(command[2]))
+for command in commands:
+    lights = getlightlist(command[1], command[2])
     for light in lights:
         operatelight(light, command[0])
 
 print(sum(1 for j in lightgrid.values() if j == 1))
-
-
-### TESTING ###        
-
-testcommands = [['toggle', '0,0', '999,999']
-               ,['off', '499,499', '500,500']
-              ]
-        
-# # Execute all of the commands
-# for command in testcommands:
-    # lights = getlightlist(getlightid(command[1]), getlightid(command[2]))
-    # for light in lights:
-        # operatelight(light, command[0])
-
-# for i in range(0,101):
-    # print(lightgrid[i])      
-
-# print(getlightlist(getlightid('499,499'), getlightid('500,500')))
-    
-# print(sum(1 for j in lightgrid.values() if j == 1))
-        
