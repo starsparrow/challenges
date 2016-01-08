12a77550b75fb3b2167d16f6ea2f43cf72118252
starsparrow <nik.spurgetis@gmail.com>
2016-01-08 11:01:48 -0500

    Just need to finish up the rest of the ops

--- advent-of-code/day7/puz1.py
+++ advent-of-code/day7/puz1.py
@@ -4,8 +4,6 @@
 import re
 import string
 
-INSTRUCTION-SET = ['AND', 'OR', 'NOT', 'RSHIFT', 'LSHIFT']
-
 # Read in the text file of instructions
 with open('input.txt', 'r') as f:
     while True:
@@ -16,75 +14,79 @@ with open('input.txt', 'r') as f:
 
 # From the list of text commands, split into [source/operation, dest] list
 commands = []
-for string in stringlist:    
-    commands.append(string.split(' -> '))
+for j in stringlist:    
+    commands.append(j.split(' -> '))
 for command in commands:
     command[0] = command[0].split(' ')        
         
 # Initialize all of our wires with signals of 0 (may not use all wires)
 wires = {}
 letters = list(string.ascii_lowercase)
+for letter in letters:
+    wires['%s' % (letter)] = 0
 for letter1 in letters:
     for letter2 in letters:
         wires['%s%s' % (letter1, letter2)] = 0
-        
+
 # If the source is a wire, translate it to its signal, or make signal an int
 def getsignal(source):
-    if re.search('[0-9]+', source):
+    if re.search('[0-9]+', str(source)):
         return int(source)
-    elif re.search('[a-z]+', source):
-        return wires[source]
+    elif re.search('[a-z]+', str(source)):
+        return int(wires[source])
 
 # Direct signal input    
-def op-direct(signal, dest):
-    pass
+def opdirect(signal, dest):
+    wires[dest] += signal
 
 # NOT operation        
-def op-not(signal, dest):
+def opnot(signal, dest):
     pass
 
 # AND operation    
-def op-and(signal1, signal2, dest):
+def opand(signal1, signal2, dest):
     pass
 
 # OR operation    
-def op-or(signal1, signal2, dest):
+def opor(signal1, signal2, dest):
     pass
 
 # LSHIFT operation    
-def op-lshift(signal1, signal2, dest):
+def oplshift(signal1, signal2, dest):
     pass
 
 # RSHIFT operation    
-def op-rshift(signal1, signal2, dest):
+def oprshift(signal1, signal2, dest):
     pass
 
-# Determine what operation to do for each command   
+# Determine what operation to do for each command
+
+# testcommands = [[[100], 'a']]
+
 for command in commands:
-    if command[0][0] == 'NOT':
+    if str(command[0][0]) == 'NOT':
         signal = getsignal(command[0][1])
         dest = command[1]
-        op-not(signal, dest)
+        opnot(signal, dest)
     elif len(command[0]) == 1:
         signal = getsignal(command[0][0])
-        dest = getsignal(command[1])
-        op-direct(signal, dest)
+        dest = command[1]
+        opdirect(signal, dest)
     elif len(command[0]) == 3:
         signal1 = getsignal(command[0][0])
         signal2 = getsignal(command[0][2])
         dest = command[1]
         operation = command[0][1]
         if operation == 'AND':
-            op-and(signal1, signal2, dest)
+            opand(signal1, signal2, dest)
         elif operation == 'OR':
-            op-or(signal1, signal2, dest)
+            opor(signal1, signal2, dest)
         elif operation == 'LSHIFT':
-            op-lshift(signal1, signal2, dest)
+            oplshift(signal1, signal2, dest)
         elif operation == 'RSHIFT':
-            op-rshift(signal1, signal2, dest)
+            oprshift(signal1, signal2, dest)
         
+print(wires['a'])
         
     
-        
-        
         
