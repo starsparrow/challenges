20cc8f015568a07ca328ce7b8db8ef13a4a511d2
starsparrow <nik.spurgetis@gmail.com>
2016-12-03 16:23:07 -0500

    Commented code. Removed invalid_triangles as it was unused for the puzzle

--- advent-of-code/2016/day3/aoc-3b.py
+++ advent-of-code/2016/day3/aoc-3b.py
@@ -3,16 +3,21 @@
 # Advent of Code - Day 3: Squares With Three Sides - Puzzle B
 # starsparrow
 
+# Read in our list of triangle sides
 f = open('input', 'r')
 rows = [line.strip() for line in f.readlines()]
 f.close()
 
+# There are three columns, so three things we need to loop through later
+# Maybe I could have done this as a dict to make things a bit cleaner?
 staging_a = []
 staging_b = []
 staging_c = []
 
+# Initialize our list of valid triangles to count at the end
 valid_triangles = []
 
+# Clean up whitespace and get all of the column data into staging lists
 for row in rows:
 	sides = row.split(" ")
 	while '' in sides:
@@ -23,6 +28,9 @@ for row in rows:
 	staging_b.append(sides[1])
 	staging_c.append(sides[2])
 
+# Go through each staging list (representing one column in original data) and
+# group them into threes. If the three sides represent a valid triangle,
+# throw them into a sublist and append to valid_triangles
 for queue in [staging_a, staging_b, staging_c]:
 	while len(queue) > 0:
 		if ((queue[0] + queue[1] > queue[2]) and
@@ -36,4 +44,5 @@ for queue in [staging_a, staging_b, staging_c]:
 		for _ in range(0,3):
 			queue.remove(queue[0])
 
+# Get our answer!
 print(len(valid_triangles))
