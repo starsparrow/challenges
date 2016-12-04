53151834da4f1178aaedf4bfa8b35f6a6f5185f9
starsparrow <nik.spurgetis@gmail.com>
2016-12-03 23:31:36 -0500

    Day 8a (incomplete)

--- advent-of-code/2015/day8/aoc-8a.py
+++ advent-of-code/2015/day8/aoc-8a.py
@@ -3,13 +3,36 @@
 # Advent of Code 2015 - Day 8: Matchsticks - Puzzle A
 # starsparrow
 
+import codecs, re
+
 f = open('input', 'r')
 strings = f.readlines()
 f.close()
 
-for string in strings:
-	escaped = string.lstrip('"').rstrip('"\n')	
-	escaped = escaped.replace('\\"', '"')
-	escaped = escaped.replace('\\\\', '\\')
-	print(escaped)
+def translate(matchobj):
+	return "x" #codecs.decode(matchobj.group(0), "hex")
 
+def clean(string):
+	cleaned = string.lstrip('"').rstrip('"\n')
+	cleaned = cleaned.replace('\\"', '"').replace('\\\\', '\\')
+	cleaned = re.sub(r'\\x..',
+					 translate,
+					 cleaned)
+	return cleaned
+
+code_charcount = 0
+mem_charcount = 0
+
+for string in strings:
+	code_charcount += len(list(string))
+	mem_charcount += len(list(clean(string)))
+	print(clean(string))
+
+print("Code character count: {}\n\
+Memory character count: {}\n\
+Difference: {}".format(
+	code_charcount,
+	mem_charcount,
+	code_charcount - mem_charcount
+	)
+)
