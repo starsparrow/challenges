6583542833b0e73f5e948af53c327080e03a69a8
starsparrow <nik.spurgetis@gmail.com>
2016-12-04 16:11:03 -0500

    Day 4 (incomplete)

---
+++ advent-of-code/2016/day4/aoc-4a.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+# Advent of Code 2016 - Day 4: Security Through Obscurity - Puzzle A
+# starsparrow
+
+# Read in our room data
+f = open('input', 'r')
+data = [line.strip() for line in f.readlines()]
+f.close()
+
+# Parse the data into a dict for easier access later
+rooms = {}
+for line in [line.split('-') for line in data]:
+    lastitem = line[len(line) - 1].split('[')
+    name = "".join(line[:line.index(line[len(line) - 1])])
+    sectorid = lastitem[0]
+    roomhash = lastitem[1].rstrip(']')
+    rooms[name] = {'sectorid': sectorid, 'hash': roomhash}
+
+def is_real_room(roomname, roomhash):
+    return(list(roomname))
+    
+    
+# Main section
+#for room in rooms:
+#print("{} {} {}".format(room,
+#                            rooms[room]['sectorid'],
+#                            rooms[room]['hash']))
+
+print(is_real_room('balsjrpaowijfeasledjfopawiejfasdf', 'asdf'))
+    
