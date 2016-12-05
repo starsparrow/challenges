5f193f8b362e58a0ef4d7542f0f18a144c70f7e6
starsparrow <nik.spurgetis@gmail.com>
2016-12-05 14:22:05 -0500

    type error with int()

--- advent-of-code/2016/day5/aoc-5b.py
+++ advent-of-code/2016/day5/aoc-5b.py
@@ -20,11 +20,11 @@ password = {
 
 hashindex = 0
 while hashindex < 10000000:
-	code = "{}{}".format(input, hashindex)
+	code = "{0}{1}".format(input, hashindex)
 	hashinput = bytes(code, 'utf-8')
 	md5hash = hashlib.md5(hashinput).hexdigest()
 	if (md5hash[0:5] == '00000'):
-		if (int(md5hash[5:6]) in range(0,8) and 
+		if (md5hash[5:6] in [0,1,2,3,4,5,6,7] and 
 			password[md5hash[5:6]] != ""):
 				password[md5hash[5:6]] = md5hash[6:7]
 				print(password)
