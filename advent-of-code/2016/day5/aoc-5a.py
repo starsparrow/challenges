a50dc9d500fdf7c1ad99242de4c78d6eecc0bfe0
starsparrow <nik.spurgetis@gmail.com>
2016-12-05 11:53:24 -0500

    w00t, Day 5 puzzle A done on first try

---
+++ advent-of-code/2016/day5/aoc-5a.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+# Advent of Code 2016 - Day 5: How About a Nice Game of Chess? - Puzzle A
+# starsparrow
+
+import hashlib
+
+input = 'ugkcyxxp'
+
+password = []
+
+hashindex = 0
+while len(password) < 8:
+	code = "{}{}".format(input, hashindex)
+	hashinput = bytes(code, 'utf-8')
+	md5hash = hashlib.md5(hashinput).hexdigest()
+	if md5hash[0:5] == '00000':
+		password.append(md5hash[5:6])
+	hashindex += 1
+
+print("".join(password))
