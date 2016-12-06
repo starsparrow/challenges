0fa875fb6abfa36653133805ded061722d0b1aff
starsparrow <nik.spurgetis@gmail.com>
2016-12-06 09:24:57 -0500

    Day 6 easy peasy

---
+++ advent-of-code/2016/day6/aoc-6a.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+# Advent of Code 2016 - Day 6: Signals and Noise - Puzzle A
+# starsparrow
+
+f = open('input', 'r')
+signal = [line.strip() for line in f.readlines()]
+f.close
+
+def most_common(list):
+	'''Return the most common letter in a list of letters'''
+
+	frequency = {}
+	for letter in list:
+		if letter in frequency.keys():
+			frequency[letter] += 1
+		else:
+			frequency[letter] = 1
+	max_freq = sorted(frequency.values(), reverse=True)[0]
+	for letter in list:
+		if frequency[letter] == max_freq:
+			return letter
+
+def enhance(signal):
+	'''Split each line into columns, find most common letter in each column
+	and join them together to get the error-corrected message'''
+
+	msg_length = len(signal[0])
+
+	columns = {}
+	for n in range(0, msg_length):
+		columns[n] = []
+
+	for message in signal:
+		letters = list(message)
+		for n in range(0, msg_length):
+			columns[n].append(letters[n])
+
+	error_corrected = []
+	for position in columns:
+		error_corrected.append(most_common(columns[position]))
+	print("".join(error_corrected))
+		
+enhance(signal)
