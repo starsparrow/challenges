78490d7c5094f641fdecf38348ad32f448e41ca3
starsparrow <nik.spurgetis@gmail.com>
2017-05-17 16:01:32 -0400

    Got it, trying again after 6 months of neglect

--- advent-of-code/2016/day07/aoc-7b.py
+++ advent-of-code/2016/day07/aoc-7b.py
@@ -1,110 +1,133 @@
-#! /usr/bin/env python
-
-# Advent of Code 2016 - Day 7: Internet Protocol Version 7 - Puzzle B
-# starsparrow
-
-import re
-
-f = open('input', 'r')
-input = [line.strip() for line in f.readlines()]
-f.close()
+#!/usr/bin/env python
 
 
-# Function definitions
+# Class definition
 
-def detect_abba(addr_part):
-    """Checks for ABBA sequence in supplied string. Returns the ABBA."""
-    charlist = list(addr_part)
-    while len(charlist) > 3:
-        if (charlist[0] == charlist[3] and
-			charlist[1] == charlist[2] and
-			charlist[0] != charlist[1]):
-                return "".join(charlist[0:4])
+class Address:
+    def __init__(self, address):
+        self.address = address
+
+    def __str__(self):
+        return self.address
+
+    def is_abba(self, segment):
+        """Check to see whether supplied four-character string is ABBA and
+        returns True/False."""
+        if len(segment) == 4:
+            letters = list(segment)
+            return (
+                       letters[0] == letters[3] and
+                       letters[0] != letters[1] and
+                       letters[1] == letters[2]
+                    )
         else:
-            charlist.remove(charlist[0])
-    return False # Only do this if a match not found in the above loop
+            raise ValueError('Address segment passed to is_abba() must be four \
+characters long.')
 
-
-def detect_aba(addr_part):
-    """Checks for ABA sequence in supplied string. Returns the ABA."""
-    charlist = list(addr_part)
-    while len(charlist) > 2:
-        if (charlist[0] == charlist[2] and
-            charlist[0] != charlist[1]):
-         return "".join(charlist[0:3])
+    def is_aba(self, segment):
+        """Check to see whether supplied four-character string is ABBA and
+        returns True/False."""
+        if len(segment) == 3:
+            letters = list(segment)
+            return (
+                       letters[0] == letters[2] and
+                       letters[0] != letters[1]
+                    )
         else:
-            charlist.remove(charlist[0])
-    return False # Only do this if a match not found in the above loop
-    
-    
-def detect_bab(aba, addr_part):
-    """For a provided ABA, check whether a corresponding BAB exists in the
-    provided string. Returns True/False.
-    """
-    charlist = list(aba)
-    bab = "".join([charlist[1], charlist[0], charlist[1]])
-    return bab in addr_part
+            raise ValueError('Address segment passed to is_aba() must be three \
+characters long.')
 
-
-def parse_address(addr):
-	"""Get normal and hypernet sequences and return them in a dict."""
-	address = addr
-	norm_seqs = []
-	hyper_seqs = []
-
-	while len(list(address)) > 0:
-		if list(address)[0] != '[':
-			m = re.search('^[a-z]+', address)
-			norm_seqs.append(m.group(0))
-		else:
-			m = re.search('\[[a-z]+\]', address)
-			hyper_seqs.append(m.group(0))
-		
-		address = list(address)
-		for n in range(0, len(m.group(0))):
-			address.remove(address[0])
-		address = "".join(address)
-
-	return {'norm_seqs': norm_seqs, 'hyper_seqs': hyper_seqs}
-
-
-def supports_tls(addr):
-	"""Make sure the hypernets don't have any ABBAs, and at least one ABBA can
-	be found in the normal address sequences. Return True if so.
-	"""
-	address_parts = parse_address(addr)
-	for seq in address_parts['hyper_seqs']:
-		if detect_abba(seq):
-			return False
-	for seq in address_parts['norm_seqs']:
-		if detect_abba(seq):
-			return True
-			
-
-def supports_ssl(addr):
-    """Make sure the supernets have at least one ABA and that there is a
-    corresponding BAB in at least one of the hypernets.
-    """
-    address_parts = parse_address(addr)
-    for normseq in address_parts['norm_seqs']:
-        if detect_aba(normseq):
-            for hyperseq in address_parts['hyper_seqs']:
-                if detect_bab(detect_aba(normseq), hyperseq):
+    def contains_abba(self, sequence):
+        """Checks a sequence for existence of an ABBA, returns True/False"""
+        if len(sequence) < 4:
+            return False
+        else:
+            start = 0
+            end = 4
+            while end < len(sequence) + 1:
+                if self.is_abba(sequence[start:end]):
                     return True
+                else:
+                    start += 1
+                    end += 1
+            return False
 
+    def get_abas(self, sequence):
+        """Returns a list of all ABAs in a given sequence"""
+        abas = []
+        if len(sequence) < 3:
+            return abas
+        else:
+            start = 0
+            end = 3
+            while end < len(sequence) + 1:
+                if self.is_aba(sequence[start:end]):
+                    abas.append(sequence[start:end])
+                start += 1
+                end += 1
+            return abas
 
-def main():
-	tls_addresses = []
-	ssl_addresses = []
-	for address in input:
-		if supports_tls(address):
-			tls_addresses.append(address)
-		if supports_ssl(address):
-		    ssl_addresses.append(address)
-	print('{} addresses support TLS.'.format(len(tls_addresses)))
-	print('{} addresses support SSL.'.format(len(ssl_addresses)))
+    def get_sequences(self):
+        """Splits an address into supernet and hypernet sequences"""
+        address = self.address
+        sequences = []
+        while len(address) > 0:
+            next_hypernet_start_index = address.find('[')
+            next_hypernet_end_index = address.find(']')
+            if next_hypernet_start_index == 0:
+                sequences.append(address[0:next_hypernet_end_index + 1])
+            elif next_hypernet_start_index == -1:
+                sequences.append(address[0:])
+            elif next_hypernet_start_index > 0:
+                sequences.append(address[0:next_hypernet_start_index])
+                
+            address = address[len(sequences[len(sequences) - 1]):]
+        
+        return sequences
+
+    def supports_tls(self):
+        """Returns if there is an ABBA in supernet but not in hypernet"""
+        hypernet_contains_abba = False
+        supernet_contains_abba = False
+        for sequence in self.get_sequences():
+            if (len(sequence) >= 6 and sequence[0] == '[' and
+                self.contains_abba(sequence)):
+                    hypernet_contains_abba = True
+            if (len(sequence) >= 4 and sequence[0] != '[' and 
+                self.contains_abba(sequence)):
+                    supernet_contains_abba = True
+        if supernet_contains_abba and not hypernet_contains_abba:
+            return True
+        else:
+            return False
+
+    def supports_ssl(self):
+        """Returns True if ABA in supernet and corresponding BAB in hypernet"""
+        hypernet_abas = []
+        supernet_abas = []
+        for sequence in self.get_sequences():
+            if sequence[0] == '[':
+                abas = self.get_abas(sequence)
+                for aba in abas:
+                    if aba not in hypernet_abas:
+                        hypernet_abas.append(aba)
+            else:
+                abas = self.get_abas(sequence)
+                for aba in abas:
+                    if aba not in supernet_abas:
+                        supernet_abas.append(aba)
+        for aba in hypernet_abas:
+            bab = '{}{}{}'.format(aba[1], aba[0], aba[1])
+            if bab in supernet_abas:
+                return True
+        return False
 
 
 # Main section
 
-main()
+with open('input.txt', 'r') as f:
+    data = f.readlines()
+    print(len([datum.rstrip() for datum in data if Address(
+        datum.rstrip()).supports_tls()]))
+    print(len([datum.rstrip() for datum in data if Address(
+        datum.rstrip()).supports_ssl()]))
