febd8167d6ec74c43b3e1351a8d3097641d275bc
starsparrow <nik.spurgetis@gmail.com>
2017-05-18 11:45:17 -0400

    Got rotate_row() and rotate_column() working

--- advent-of-code/2016/day08/aoc-8a.py
+++ advent-of-code/2016/day08/aoc-8a.py
@@ -3,10 +3,8 @@
 # Advent of Code 2016 - Day 8: Two-Factor Authentication - Puzzle A
 # starsparrow
 
-f = open('input', 'r')
-instructions = [line.strip() for line in f.readlines()]
-f.close()
 
+# Class definition
 
 class Display:
 	"""Pixels! Woohoo!"""
@@ -29,44 +27,55 @@ class Display:
 		for pixel in pixels_to_turn_on:
 			self.pixels[pixel] = '#'
 
-	# def rotate_row(self, row):
-	# 	"""Shift pixels in row right 1 pixel"""
-	# 	active_pixels = sorted([pixel for pixel in self.pixels if pixel[1] == row and
-	# 		self.pixels[pixel] == '#'])
-	# 	del active_pixels[0]
-	# 	last_pixel = active_pixels[len(active_pixels) - 1]
-	# 	next_x = last_pixel[0] + 1 if last_pixel[0] < self.resolution[0] else 0
-	# 	active_pixels.append((next_x, last_pixel[1]))
-	# 	return active_pixels
-
 	def rotate_row(self, row):
 		"""Shift pixels in row right 1 pixel"""
 		active_pixels = sorted([pixel for pixel in self.pixels if pixel[1] == row and
 			self.pixels[pixel] == '#'])
 		updated_pixels = []
 		for pixel in active_pixels:
-			if pixel[0] < self.resolution[0]:
-				new_x = pixel[0] + 1
+			if pixel[0] < self.resolution[0] - 1:
+				new_y = pixel[0] + 1
 			else:
-				new_x = (pixel[0] + 1) % self.resolution[0]
-			updated_pixels.append([(new_x, pixel[1])])
-			
-		return updated_pixels
+				new_y = (pixel[0] + 1) % self.resolution[0]
+			updated_pixels.append((new_y, pixel[1]))
+		for pixel in active_pixels:
+			self.pixels[pixel] = '.'
+		for pixel in updated_pixels:
+			self.pixels[pixel] = '#'
 
 	def rotate_column(self, col):
 		"""Shift pixels in column down 1 pixel"""
-		pass
+		active_pixels = sorted([pixel for pixel in self.pixels if pixel[0] == col and
+			self.pixels[pixel] == '#'])
+		updated_pixels = []
+		for pixel in active_pixels:
+			if pixel[1] < self.resolution[1] - 1:
+				new_x = pixel[1] + 1
+			else:
+				new_x = (pixel[1] + 1) % self.resolution[1]
+			updated_pixels.append((pixel[0], new_x))
+		for pixel in active_pixels:
+			self.pixels[pixel] = '.'
+		for pixel in updated_pixels:
+			self.pixels[pixel] = '#'
 
 	def show(self):
 		"""Show the display."""
 		return '\n'.join(''.join([self.pixels[(x,y)] for x in range(0, self.resolution[0])])
 			for y in range(0, self.resolution[1]))
 
+
+# Main section
+
+f = open('input', 'r')
+instructions = [line.strip() for line in f.readlines()]
+f.close()
+
 screen = Display((50,6))
 screen.rect(4,2)
-
-for x in range(40):
-	print(screen.rotate_row(0))
-
+for n in range(11):
+	screen.rotate_column(0)
+for n in range(5):
+	screen.rotate_row(5)
 print('\n')
 print(screen.show())
